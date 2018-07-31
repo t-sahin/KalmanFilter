b280f53c22748a65f76cb9928d823bce3391fbb4
t-sahin <38439986+t-sahin@users.noreply.github.com>
2018-07-31 15:53:56 -0400

    Corrected variable name error.

--- SamplePaths/Path1.py
+++ SamplePaths/Path1.py
@@ -18,7 +18,7 @@ y_3 = func_sin(x_3)
 x_path1 = np.append(x_1, np.append(x_2, x_3)) 
 y_path1 = np.append(y_1, np.append(y_2, y_3))
 
-x_path1_scaled, y_path2_scaled = PathCalcs.scale_to(x_path1, y_path1, 16, 9, 1)
+x_path1_scaled, y_path1_scaled = PathCalcs.scale_to(x_path1, y_path1, 16, 9, 1)
 position_measured_path1 = PathCalcs.measurement_noise(x_path1_scaled, y_path1_scaled, 0.2, 0.2)
 
 np.savetxt('KF_Path1.txt', position_measured_path1)
